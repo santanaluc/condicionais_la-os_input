506ad013e9dbada0f89fc2f868281320759760de
santanaluc <lucas.s.s.silva@hotmail.com>
2021-03-09 11:40:13 -0300

    Implementando tabuada

--- src/app/tabuada/tabuada.component.ts
+++ src/app/tabuada/tabuada.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-tabuada',
-  templateUrl: './tabuada.component.html',
-  styleUrls: ['./tabuada.component.css']
+  selector: "app-tabuada",
+  templateUrl: "./tabuada.component.html",
+  styleUrls: ["./tabuada.component.css"]
 })
 export class TabuadaComponent implements OnInit {
+  n: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.n = 1;
   }
 
+  getTabuada() {
+    let i: number = 0;
+    while (i <= 10) {
+      let conta: number = i * this.n;
+      return conta;
+    }
+  }
+
+  getTabuadaList() {
+    let list: number[] = [];
+
+    for (let i = 0; i <= 10; i++) list.push(this.getTabuada());
+
+    return list;
+  }
+
+  ngOnInit() {}
 }
