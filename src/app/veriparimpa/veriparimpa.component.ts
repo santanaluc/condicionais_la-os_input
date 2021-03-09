ecc33b5fac96f48fd1381ff60fcb11f367c43d88
santanaluc <lucas.s.s.silva@hotmail.com>
2021-03-09 12:26:13 -0300

    Implementando verificação de Par/Ímpar

--- src/app/veriparimpa/veriparimpa.component.ts
+++ src/app/veriparimpa/veriparimpa.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-veriparimpa',
-  templateUrl: './veriparimpa.component.html',
-  styleUrls: ['./veriparimpa.component.css']
+  selector: "app-veriparimpa",
+  templateUrl: "./veriparimpa.component.html",
+  styleUrls: ["./veriparimpa.component.css"]
 })
 export class VeriparimpaComponent implements OnInit {
+  numero = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.numero;
   }
 
+  getNumber() {
+    return this.numero;
+  }
+
+  isPar() {
+    // if (this.getNumber() % 2 == 0) {
+    //   return "Par";
+    // }
+    return this.getNumber() % 2 == 0;
+  }
+
+  ngOnInit() {}
 }
